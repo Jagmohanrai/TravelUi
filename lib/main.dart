a8f83672580bd1a9d67a40a3b2125ae81b6c3034
Jagmohan Rai <aahhansingh@gmail.com>
2020-11-16 14:14:58 +0530

    screenshot added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:travelui/Screens/secondpage.dart';
 import 'Screens/homepage.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      home: DetailPage(),
     );
   }
 }
